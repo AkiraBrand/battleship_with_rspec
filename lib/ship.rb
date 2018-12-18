1cdcfb1692a674fe6846666ebb9b83e574dccf76
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-12-18 16:22:47 -0700

    wip

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,9 @@
 class Ship
+
+  attr_reader :name
+
+  def initialize(name, length)
+    @name = name
+  end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,5 +1,8 @@
 require 'ship'
 
 describe Ship do
-
+  it "has a name" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.name).to eq("Cruiser")
+ end
 end
